57e331a8a3500e5a28bc384ded0cea54c9b822a0
Gaby Rodriguez <leapinlab@gmail.com>
2023-09-19 09:58:16 -0600

    newNote() is finished and working

--- js/functions.js
+++ js/functions.js
@@ -29,32 +29,60 @@
         
         /*add the div to the left bar*/
         document.getElementById("leftBar").appendChild(node);
+
+        /*create a new note to go with it*/
+        newNote();
     }
 
     /*creates a new note*/
     function newNote(){
-        /*create the new div*/
-        const node = document.createElement("div");
-        
-        /*create the note title*/
-        const textnode = document.createTextNode("New Note");
-        
-        /*create the trash image and add its classes/attributes*/
-        const trash = document.createElement("img");
-        trash.setAttribute("src", "/images/trash.png");
-        trash.setAttribute("onclick", "deleteNote(" + i + ")")
-        trash.classList.add("self-center");
-                
-        /*add the title and image*/
-        node.appendChild(textnode);
-        node.appendChild(trash);
+        /*create the new note div*/
+        const note = document.createElement("div");
+
+        /*create the title div*/
+        const titleDiv = document.createElement("div")
+
+            /*create the note title*/
+            const textnode = document.createTextNode("New Note");
+
+            /*create the buttons div*/
+            const buttons = document.createElement("div");
+
+            /*add the buttons attributes/classes*/
+            buttons.classList.add("buttons", "flex", "justify-between");
+
+                /*create the add image and add its classes/attributes*/
+                const add = document.createElement("img");
+                add.setAttribute("src", "/images/add.png");
+                add.setAttribute("onclick", "newToDo(" + i + ")")
+                add.classList.add("self-center");
+
+                /*create the close image and add its classes/attributes*/
+                const close = document.createElement("img");
+                close.setAttribute("src", "/images/close.png");
+                close.setAttribute("onclick", "closeNote(" + i + ")")
+                close.classList.add("self-center");
+
+                /*add the images to the buttons div*/
+                buttons.appendChild(add);
+                buttons.appendChild(close);
+                    
+            /*add the title and buttons*/
+            titleDiv.appendChild(textnode);
+            titleDiv.appendChild(buttons);
+
+        /*add the title div's attributes/classes*/
+        titleDiv.classList.add("noteTitle", "flex", "justify-between");    
+
+        /*add the div to the note*/
+        note.appendChild(titleDiv);
         
         /*add the note's classes/attributes*/
-        node.classList.add("noteTab", "flex", "justify-between")
-        node.setAttribute("id", "noteTab" + i);
+        note.classList.add("note");
+        note.setAttribute("id", "note" + i);
                 
-        /*add the div to the left bar*/
-        document.getElementById("leftBar").appendChild(node);
+        /*add the div to the right area*/
+        document.getElementById("rightArea").appendChild(note);
     }
 
     /*creates a new toDo*/
