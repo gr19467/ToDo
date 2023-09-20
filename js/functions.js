d2c5e002d332e3d42c905e13016d5c3f20bccb36
Gaby Rodriguez <leapinlab@gmail.com>
2023-09-20 08:58:23 -0600

    note dragging is working

--- js/functions.js
+++ js/functions.js
@@ -86,6 +86,7 @@
         /*add the note's classes/attributes*/
         note.classList.add("note");
         note.setAttribute("id", "note" + i);
+        note.setAttribute("onmouseover", "dragElement(note" + i + ")");
                 
         /*add the div to the right area*/
         document.getElementById("rightArea").appendChild(note);
@@ -166,3 +167,40 @@ function changeTitle(num){
     var newTitle = document.getElementById("noteTab" + num).textContent;
     document.getElementById("title" + num).textContent = newTitle;
 }
+
+/*div moving*/
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        
+        elmnt.onmousedown = dragMouseDown;
+    
+        function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+        }
+    
+        function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+    
+        function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+        }
+    }
